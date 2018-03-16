4e4cb75b398e767a59bbb10c530930d2558dbeb1
wikal pratama <wikal.wp@email.unikom.ac.id>
2018-03-16 20:02:28 +0700

    nambah file ke learncode

---
+++ js/gayaku.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.material-button-toggle').on("click", function () {
+        $(this).toggleClass('open');
+        $('.option').toggleClass('scale-on');
+    });
+});
